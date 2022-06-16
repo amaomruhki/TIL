7fa1b277f01a2d3b472c2c0ebce3420e9f388260
mai <amaomruhki@gmail.com>
2022-06-16 20:06:44 +0900

    モック完成

--- myfirstApp/src/App.jsx
+++ myfirstApp/src/App.jsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState(['くろしろにごはん','くろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはん']);
+
   return (
     <>
       <header>
           <h1>todos</h1>  
       </header>
 
-      <div className="input-area container">
-        <input type="text" placeholder="Enter your todo" />
-        <button className="add-btn">+</button>
+      <div className="input-area">
+        <div className="input-container">
+          <input type="text" placeholder="Enter your todo" value={todoText} />
+          <button className="add-btn">+</button>
+        </div>
       </div>
 
-      <div className="added-area container">
+      <div className="todos-area">
         <ul>
-          <li>
-            <div className="list-row">
-              <input type="checkbox" id="checkbox" value="" />
-              <p>くろしろにごはん</p>
-              <button>x</button>
-            </div>
-          </li>
-          <li>
-            <div className="list-row">
-              <input type="checkbox" id="checkbox" value="" />
-              <p>ちゅーる2000個献上</p>
-              <button>x</button>
-            </div>
-          </li>
+          {todos.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <select name="status" id="status">
+                    <option value="1">Waiting</option>
+                    <option value="2">Working</option>
+                    <option value="3">Completed</option>
+                    <option value="4">Pending</option>
+                  </select>
+                    <p>{todo}</p>
+                  <button className="edit-btn"><EditIcon /></button>
+                  <button className="delete-btn"><DeleteForeverIcon /></button>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </div>
 
