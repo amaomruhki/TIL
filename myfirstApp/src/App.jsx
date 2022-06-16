531c35545486a30b82c0e1c070a10a9aeb4805bf
mai <amaomruhki@gmail.com>
2022-06-16 21:41:20 +0900

    追加＆削除機能実装

--- myfirstApp/src/App.jsx
+++ myfirstApp/src/App.jsx
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./index.css";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 export const App = () => {
-  const [todoText, setTodoText] = useState('');
-  const [todos, setTodos] = useState(['くろしろにごはん','くろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはんくろしろにごはん']);
+  //入力欄のtodoのステート
+  const [todoText, setTodoText] = useState("");
+  //追加済みtodoのステート 
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...todos, { id: todos.length + 1, text: todoText}];
+    setTodos(newTodos)
+    setTodoText("");
+  }
+  const handleDeleteClick = (id) => {
+    const removeItem = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(removeItem);
+  } 
 
   return (
     <>
@@ -15,8 +42,8 @@ export const App = () => {
 
       <div className="input-area">
         <div className="input-container">
-          <input type="text" placeholder="Enter your todo" value={todoText} />
-          <button className="add-btn">+</button>
+          <input type="text" placeholder="Enter your todo" value={todoText} onChange={onChangeTodoText} />
+          <button className="add-btn" onClick={onClickAdd}>+</button>
         </div>
       </div>
 
@@ -24,7 +51,7 @@ export const App = () => {
         <ul>
           {todos.map((todo) => {
             return (
-              <li key={todo}>
+              <li key={todo.id}>
                 <div className="list-row">
                   <select name="status" id="status">
                     <option value="1">Waiting</option>
@@ -32,9 +59,9 @@ export const App = () => {
                     <option value="3">Completed</option>
                     <option value="4">Pending</option>
                   </select>
-                    <p>{todo}</p>
+                    <p>{todo.text}</p>
                   <button className="edit-btn"><EditIcon /></button>
-                  <button className="delete-btn"><DeleteForeverIcon /></button>
+                  <button className="delete-btn" onClick={() => handleDeleteClick(todo.id)}><DeleteForeverIcon /></button>
                 </div>
               </li>
             )
