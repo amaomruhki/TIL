24d2121b85c78c29303e37e89e866616085392aa
mai <amaomruhki@gmail.com>
2022-07-06 09:20:47 +0900

    app complete

--- registertest/src/Mypage.tsx
+++ registertest/src/Mypage.tsx
@@ -1,22 +1,68 @@
-import React from "react";
-import { Box, Flex, Heading, Divider, Stack, Button } from "@chakra-ui/react";
+import React, { useState, useEffect, useTransition } from "react";
+import {
+	Text,
+	Box,
+	Flex,
+	Heading,
+	Divider,
+	Stack,
+	Button,
+} from "@chakra-ui/react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./FirebaseConfig";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const Mypage = () => {
+	const [user, setUser] = useState<any | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setLoading(false);
+		});
+	}, []);
+
+	const navigate = useNavigate();
+
+	const logout = async () => {
+		await signOut(auth);
+		navigate("/login/");
+	};
+
 	return (
 		<>
-			<Flex align="center" justify="center" height="100vh">
-				<Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
-					<Heading as="h1" size="lg" textAlign="center">
-						マイページ
-					</Heading>
-					<Divider my={4} />
-					<Stack spacing={6} py={4} px={10}>
-						<Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-							ログアウト
-						</Button>
-					</Stack>
-				</Box>
-			</Flex>
+			{!loading && (
+				<>
+					{!user ? (
+						<Navigate to={`/login/`} />
+					) : (
+						<>
+							<Flex align="center" justify="center" height="100vh">
+								<Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+									<Heading as="h1" size="lg" textAlign="center">
+										マイページ
+									</Heading>
+									<Divider my={4} />
+									<Stack spacing={6} py={4} px={10}>
+										<Text fontSize="md" textAlign="center">
+											{user?.email}
+										</Text>
+										<Button
+											onClick={logout}
+											bg="teal.400"
+											color="white"
+											_hover={{ opacity: 0.8 }}
+										>
+											ログアウト
+										</Button>
+									</Stack>
+								</Box>
+							</Flex>
+						</>
+					)}
+				</>
+			)}
 		</>
 	);
 };
--- registertest/src/Register.tsx
+++ registertest/src/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Box,
 	Flex,
@@ -9,44 +9,97 @@ import {
 	FormControl,
 	FormLabel,
 	Input,
+	Text,
 } from "@chakra-ui/react";
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "./FirebaseConfig";
+import { Navigate, Link } from "react-router-dom";
 
 const Register = () => {
 	const [registerEmail, setRegisterEmail] = useState("");
 	const [registerPassword, setRegisterPassword] = useState("");
 
+	const onClickSubmit = async (e: any) => {
+		e.preventDefault();
+
+		try {
+			await createUserWithEmailAndPassword(
+				auth,
+				registerEmail,
+				registerPassword
+			);
+		} catch (error) {
+			alert("正しく入力してください");
+		}
+	};
+
+	const [user, setUser] = useState<any | null>(null);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+		});
+	}, []);
+
 	return (
 		<>
-			<Flex align="center" justify="center" height="100vh">
-				<Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
-					<Heading as="h1" size="lg" textAlign="center">
-						新規登録
-					</Heading>
-					<Divider my={4} />
-					<FormControl>
-						<Stack spacing={6} py={4} px={10}>
-							<Box>
-								<FormLabel htmlFor="email">メールアドレス</FormLabel>
-								<Input
-									name="email"
-									type="email"
-									value={registerEmail}
-									onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-										setRegisterEmail(e.target.value)
-									}
-								/>
-							</Box>
-							<Box>
-								<FormLabel htmlFor="password">パスワード</FormLabel>
-								<Input name="password" type="password" />
-							</Box>
-							<Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-								登録する
-							</Button>
-						</Stack>
-					</FormControl>
-				</Box>
-			</Flex>
+			{user ? (
+				<Navigate to={`/`} />
+			) : (
+				<>
+					<Flex align="center" justify="center" height="100vh">
+						<Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+							<Heading as="h1" size="lg" textAlign="center">
+								新規登録
+							</Heading>
+							<Divider my={4} />
+							<FormControl>
+								<Stack spacing={6} py={4} px={10}>
+									<Box>
+										<FormLabel htmlFor="email">メールアドレス</FormLabel>
+										<Input
+											name="email"
+											type="email"
+											value={registerEmail}
+											onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+												setRegisterEmail(e.target.value)
+											}
+										/>
+									</Box>
+									<Box>
+										<FormLabel htmlFor="password">パスワード</FormLabel>
+										<Input
+											name="password"
+											type="password"
+											value={registerPassword}
+											onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+												setRegisterPassword(e.target.value)
+											}
+										/>
+									</Box>
+									<Button
+										onClick={onClickSubmit}
+										bg="teal.400"
+										color="white"
+										_hover={{ opacity: 0.8 }}
+									>
+										登録する
+									</Button>
+									<Text fontSize="md">
+										ログインは
+										<Link to={`/login/`}>
+											<Text as="u">こちら</Text>
+										</Link>
+									</Text>
+								</Stack>
+							</FormControl>
+						</Box>
+					</Flex>
+				</>
+			)}
 		</>
 	);
 };
