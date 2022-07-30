dea9dac0203b254a02222ed331767971def236bd
mai <amaomruhki@gmail.com>
2022-07-30 21:37:39 +0900

    create-app

---
+++ idobatakaigi/src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDtrq6Cz1CfjTkey28aBMYtvYlBweWjQjw",
+	authDomain: "idobatakaigi-with-9646.firebaseapp.com",
+	databaseURL: "https://idobatakaigi-with-9646-default-rtdb.firebaseio.com/",
+	projectId: "idobatakaigi-with-9646",
+	storageBucket: "idobatakaigi-with-9646.appspot.com",
+	messagingSenderId: "240431481755",
+	appId: "1:240431481755:web:e4b0821907c958006f973e",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const messageRef = database.ref("messages");
+
+export const pushMessage = ({ name, text }) => {
+	messageRef.push({ name, text });
+};
