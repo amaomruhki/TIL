efd231458cb393fa6cd0f7c8ed5ef6e951d9fc75
mai <amaomruhki@gmail.com>
2022-08-03 20:32:00 +0900

    todo list form

--- firebase-react-hosting/src/App.js
+++ firebase-react-hosting/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container, Form, Input, InputGroup, Button } from "reactstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addTodo(value);
+	};
+
+	const addTodo = (text) => {
+		const newTodos = [...todos, text];
+		setTodos(newTodos);
+		console.log(newTodos);
+	};
+
+	return (
+		<div className="App">
+			<Container>
+				<h1 className="mt-4">Todo List</h1>
+				<Form onSubmit={handleSubmit}>
+					<InputGroup>
+						<Input
+							type="text"
+							value={value}
+							onChange={(e) => setValue(e.target.value)}
+						/>
+						<Button type="submit" color="primary">
+							Add
+						</Button>
+					</InputGroup>
+				</Form>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
